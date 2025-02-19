96cfc481fdc8be601299a68e28d6acceec726e9c
Fabrizio Anichini <fabrizioanichini@proton.me>
2025-02-19 18:45:00 +0100

    core packages for nvim and nerd font

--- dotfiles/.config/wezterm/wezterm.lua
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'Dracula'
 
-config.font = wezterm.font('JetBrains Mono')
+config.font = wezterm.font('CaskaydiaCove Nerd Font Mono')
 
 config.font_size = 10 
 
