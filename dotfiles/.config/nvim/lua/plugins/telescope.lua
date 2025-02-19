10a3e9c8edcc126d8d2ff440f485049861867215
Fabrizio Anichini <fabrizioanichini@proton.me>
2025-02-19 19:04:36 +0100

    telescope hidden files

--- dotfiles/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles/.config/nvim/lua/plugins/telescope.lua
@@ -2,66 +2,70 @@
 -- =                    plugins/telescope.lua                           =
 -- ======================================================================
 return {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    event = 'VimEnter',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-          return vim.fn.executable('make') == 1
-        end,
-      },
-      'nvim-telescope/telescope-ui-select.nvim',
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-    },
-    config = function()
-      local telescope = require('telescope')
-      local builtin = require('telescope.builtin')
-  
-      telescope.setup({
-        extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
-        },
-      })
-  
-      pcall(telescope.load_extension, 'fzf')
-      pcall(telescope.load_extension, 'ui-select')
-  
-      -- Keymaps
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Search Help' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'Search Keymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Search Files' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = 'Search Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Search Word' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Search Grep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'Search Diagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = 'Search Resume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = 'Search Recent Files' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Search Buffers' })
-  
-      vim.keymap.set('n', '<leader>/', function()
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown({
-          winblend = 10,
-          previewer = false,
-        }))
-      end, { desc = 'Fuzzily search in current buffer' })
-  
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep({
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        })
-      end, { desc = 'Search in open files' })
-  
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files({ cwd = vim.fn.stdpath('config') })
-      end, { desc = 'Search Neovim Config' })
-    end,
-  }
-  
-  
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	event = "VimEnter",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
+		"nvim-telescope/telescope-ui-select.nvim",
+		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+	},
+	config = function()
+		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
+
+		telescope.setup({
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown(),
+				},
+			},
+		})
+
+		pcall(telescope.load_extension, "fzf")
+		pcall(telescope.load_extension, "ui-select")
+
+		-- Keymaps
+		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Search Help" })
+		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Search Keymaps" })
+		vim.keymap.set("n", "<leader>sf", function()
+			builtin.find_files({
+				hidden = true,
+				no_ignore = false,
+				file_ignore_patterns = { "^.git/" },
+			})
+		end, { desc = "Search Files (including hidden)" })
+		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "Search Telescope" })
+		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "Search Word" })
+		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search Grep" })
+		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Search Diagnostics" })
+		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "Search Resume" })
+		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = "Search Recent Files" })
+		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Search Buffers" })
+
+		vim.keymap.set("n", "<leader>/", function()
+			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+				winblend = 10,
+				previewer = false,
+			}))
+		end, { desc = "Fuzzily search in current buffer" })
+
+		vim.keymap.set("n", "<leader>s/", function()
+			builtin.live_grep({
+				grep_open_files = true,
+				prompt_title = "Live Grep in Open Files",
+			})
+		end, { desc = "Search in open files" })
+
+		vim.keymap.set("n", "<leader>sn", function()
+			builtin.find_files({ cwd = vim.fn.stdpath("config") })
+		end, { desc = "Search Neovim Config" })
+	end,
+}
